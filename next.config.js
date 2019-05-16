d220ec5746cd005df0587eabb57804ab307d57e7
BENZ <naruepanart1201@gmail.com>
2019-05-16 11:23:33 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images')
 const withCSS = require('@zeit/next-css')
+const withFonts = require('next-fonts');
 
 module.exports = withPlugins([
   [withImages],
   [withCSS],
+  [withFonts],
 ]);
